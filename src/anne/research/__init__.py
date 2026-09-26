6aac6c960dbbe44b326ef80426b60d0a3604a965
mustafa gökhan yılmaz <mgy421977@gmail.com>
2026-09-26 03:11:14 +0300

    Export research evidence package

--- src/anne/research/__init__.py
+++ src/anne/research/__init__.py
@@ -1,5 +1,6 @@
 """ANNE research interfaces and evidence-preserving research workflows."""
 
+from .evidence_package import ResearchEvidencePackage, apply_evidence_package
 from .general import GeneralResearchEngine, ResearchMission, ResearchReport
 from .price_intelligence import (
     PriceExtractor,
@@ -17,6 +18,7 @@ from .web_search import (
 )
 
 __all__ = [
+    "ResearchEvidencePackage", "apply_evidence_package",
     "GeneralResearchEngine", "ResearchMission", "ResearchReport",
     "PriceExtractor", "PriceObservation", "PriceResearchRequest",
     "PriceResearchRunner", "PriceVerification", "WebSearchProvider",
